e29e25ca54d1b6e569c45acb936181a18fc3b3f6
Nanami91 <nana.1999_04_26.ina@me.com>
2021-05-09 16:04:46 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :shipping_days_id
+    validates :price
+
+    validates :image, unless: :was_attached?
+    def was_attached?
+     self.image.attached?
+    end
+
+  end
 end
