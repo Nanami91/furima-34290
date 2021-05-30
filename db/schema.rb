4e0c3a33e40b50289937728c3708fac82d68b8b4
Nanami91 <nana.1999_04_26.ina@me.com>
2021-05-30 13:16:45 +0900

    カラムの修正

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_175733) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "home_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "item_purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
