1416202b5bac816ae690b7ef1ba621e1a36da6de
Nanami91 <nana.1999_04_26.ina@me.com>
2021-05-30 13:30:56 +0900

    クレジットカードの実装

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,4 +1,6 @@
-class ItemPurchasesController < ApplicationController
+class ItemPurchasesController < ItemsController
+  before_action :authenticate_user!
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @item = Item.find(params[:item_id])
@@ -9,7 +11,7 @@ class ItemPurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @address_purchase = AddressPurchase.new(purchase_params)
     if @address_purchase.valid?
-      # binding.pry
+      pay_item
       @address_purchase.save
       redirect_to root_path
     else
@@ -20,7 +22,25 @@ class ItemPurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:address_purchase).permit(:post_code, :prefecture_id, :city, :home_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:address_purchase).permit(:post_code, :prefecture_id, :city, :home_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if @item.item_purchase.present?
+      redirect_to root_path
+    elsif current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
 end
